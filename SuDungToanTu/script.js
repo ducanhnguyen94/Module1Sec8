0958d3712379eb4ece3632240b1299de387d3fe6
ducanhnguyen94 <ndanh4198@gmail.com>
2025-05-18 11:00:09 +0700

    BaiTap

---
+++ SuDungToanTu/script.js
@@ -0,0 +1,10 @@
+let width = +prompt('Enter width:');
+let length = +prompt('Enter length:');
+
+let area = width * length;
+
+document.write('Width: ' + width)
+document.write('<br>')
+document.write('Length: ' + length)
+document.write('<br>')
+document.write('Area: ' + area)
