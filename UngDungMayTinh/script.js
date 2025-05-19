88e7ea6af37714e8fab3eba5a459c2d24487a13a
ducanhnguyen94 <ndanh4198@gmail.com>
2025-05-19 20:57:09 +0700

    BaiTap

---
+++ UngDungMayTinh/script.js
@@ -0,0 +1,29 @@
+const result = document.getElementById('result');
+
+let hienThi = "";
+let phepTinh = "";
+
+function hienThiPhepTinh(event) {
+    if (event.target.value === '%') {
+        hienThi += "%";
+        phepTinh += "/100";       
+    } else {
+        hienThi += event.target.value;
+        phepTinh += event.target.value;    
+    };
+
+    result.innerHTML = hienThi;
+}
+
+function clearData() {
+    phepTinh = "";
+    hienThi = "";
+    result.innerHTML = 0;
+}
+
+function tinh() {
+    result.innerHTML = eval(phepTinh);
+    console.log(phepTinh);
+    phepTinh = "";
+    hienThi = "";
+}
